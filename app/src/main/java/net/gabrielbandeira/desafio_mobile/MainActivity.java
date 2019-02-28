7efd39bb029073454917090fbedb8fca81dd5f55
Gabriel Bandeira <gabrielbandeiracarneiro@gmail.com>
2019-02-28 17:17:00 -0300

    alter string

--- app/src/main/java/net/gabrielbandeira/desafio_mobile/MainActivity.java
+++ app/src/main/java/net/gabrielbandeira/desafio_mobile/MainActivity.java
@@ -28,10 +28,10 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
                     return true;
             }
             return false;
