55ae42fbf8269132128e515bb9dc5ce458f5169f
Gabriel Bandeira <gabrielbandeiracarneiro@gmail.com>
2019-02-28 14:55:36 -0300

    alter icon

--- app/src/main/java/net/gabrielbandeira/desafio_mobile/MainActivity.java
+++ app/src/main/java/net/gabrielbandeira/desafio_mobile/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
             }
             return false;
         }
@@ -42,9 +45,7 @@ public class MainActivity extends AppCompatActivity
 
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-
-  //     navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
